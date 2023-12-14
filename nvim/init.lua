f614a288d54945c67f6ff82c9659f5a45b850b83
Steven Manos <smanos@manosBookAir.local>
2023-12-14 23:58:06 +1100

    More Features

--- nvim/init.lua
+++ nvim/init.lua
@@ -418,6 +418,18 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'voldikss/vim-floaterm',
+    keys = {
+      { '<Leader>2', ':FloatermToggle<CR>' },
+      { '<Leader>2', '<C-\\><C-n>:FloatermToggle<CR>', mode = 't' },
+    },
+    cmd = { 'FloatermToggle' },
+    init = function()
+      vim.g.floaterm_width = 0.8
+      vim.g.floaterm_height = 0.8
+    end,
+  },
 
   {
     -- Highlight, edit, and navigate code
